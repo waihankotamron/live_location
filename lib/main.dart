5ff65708fa915f68032831c4b882e1652251b6f9
WAI_HAN_KO <waihanko.dev@gmail.com>
2021-09-02 10:51:26 +0630

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,371 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:live_location/models/pin_pill_info.dart';
+import 'package:location/location.dart';
+import 'dart:async';
+
+const double CAMERA_ZOOM = 16;
+const double CAMERA_TILT = 0;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(16.82866416441481, 96.15513903995169);
+const LatLng DEST_LOCATION = LatLng(16.802160123421412, 96.13973003512095);
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterBackgroundService.initialize(onStart, foreground: false);
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MapPage()));
+}
+
+class MapPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MapPageState();
+}
+
+class MapPageState extends State<MapPage> {
+  GoogleMapController _controller;
+  Set<Marker> _markers = Set<Marker>();
+  StreamSubscription _locationSubscription;
+
+// for my custom marker pins
+  BitmapDescriptor driverIcon;
+  LocationData currentLocation;
+
+// a reference to the destination location
+  LocationData destinationLocation = LocationData.fromMap({
+    "latitude": DEST_LOCATION.latitude,
+    "longitude": DEST_LOCATION.longitude
+  });
+
+// wrapper around the location API
+  Location _locationTracker = Location();
+  double pinPillPosition = -100;
+
+  @override
+  void initState() {
+    super.initState();
+    setSourceAndDestinationIcons().then((value) {
+      setState(() {});
+    });
+    // set the initial location
+  }
+
+  @override
+  void dispose() {
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+    super.dispose();
+  }
+
+  Future setSourceAndDestinationIcons() async {
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(devicePixelRatio: 2.0), 'assets/driving_pin.png')
+        .then((icon) {
+      driverIcon = icon;
+    });
+
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(devicePixelRatio: 2.0), 'assets/driving_pin.png')
+        .then((sourceIcon) {
+      _markers.add(Marker(
+          markerId: MarkerId('sourcePin'),
+          position: SOURCE_LOCATION,
+          icon: sourceIcon));
+    });
+
+    BitmapDescriptor.fromAssetImage(ImageConfiguration(devicePixelRatio: 2.0),
+            'assets/destination_map_marker.png')
+        .then((destinationIcon) {
+      _markers.add(Marker(
+          markerId: MarkerId('destPin'),
+          position: DEST_LOCATION,
+          icon: destinationIcon));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CameraPosition initialCameraPosition = CameraPosition(
+        zoom: CAMERA_ZOOM,
+        tilt: CAMERA_TILT,
+        bearing: CAMERA_BEARING,
+        target: SOURCE_LOCATION);
+    if (currentLocation != null) {
+      initialCameraPosition = CameraPosition(
+          target: LatLng(currentLocation.latitude, currentLocation.longitude),
+          zoom: CAMERA_ZOOM,
+          tilt: CAMERA_TILT,
+          bearing: CAMERA_BEARING);
+    }
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+              myLocationEnabled: true,
+              compassEnabled: true,
+              tiltGesturesEnabled: false,
+              markers: _markers,
+              mapType: MapType.normal,
+              initialCameraPosition: initialCameraPosition,
+              onTap: (LatLng loc) {
+                pinPillPosition = -100;
+              },
+              onMapCreated: (GoogleMapController controller) {
+                controller.setMapStyle(Utils.mapStyles);
+                _controller = controller;
+              }),
+          // MapPinPillComponent(
+          //     pinPillPosition: pinPillPosition,
+          //     currentlySelectedPin: currentlySelectedPin)
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.location_searching),
+          onPressed: () async {
+            getCurrentLocation();
+          }),
+    );
+  }
+
+  void getCurrentLocation() async {
+    try {
+      var location = await _locationTracker.getLocation();
+
+      updateDriverLocation(location);
+
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+      _locationSubscription =
+          _locationTracker.onLocationChanged().listen((newLocalData) {
+        if (_controller != null) {
+          _controller.animateCamera(
+            CameraUpdate.newCameraPosition(
+              new CameraPosition(
+                  target: LatLng(newLocalData.latitude, newLocalData.longitude),
+                  zoom: CAMERA_ZOOM,
+                  tilt: CAMERA_TILT,
+                  bearing: CAMERA_BEARING),
+            ),
+          );
+          updateDriverLocation(newLocalData);
+        }
+      });
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
+  void updateDriverLocation(LocationData newLocalData) async {
+    LatLng latLng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    var isRunning = await FlutterBackgroundService().isServiceRunning();
+    if (isRunning) {
+      FlutterBackgroundService().sendData(
+        {
+          "lat": latLng.latitude,
+          "lng": latLng.longitude,
+        },
+      );
+    } else {
+      FlutterBackgroundService.initialize(onStart);
+    }
+    this.setState(() {
+      _markers.removeWhere((m) => m.markerId.value == 'driverMarker');
+
+      _markers.add(Marker(
+          markerId: MarkerId("driverMarker"),
+          position: latLng,
+          icon: driverIcon));
+    });
+  }
+}
+
+class Utils {
+  static String mapStyles = '''[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]''';
+}
+
+void onStart() {
+  double lat,lng;
+  WidgetsFlutterBinding.ensureInitialized();
+  final service = FlutterBackgroundService();
+
+  service.onDataReceived.listen((event) {
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    } else {
+      lat = event["lat"];
+      lng = event["lng"];
+    }
+  });
+
+  Timer.periodic(Duration(seconds: 4), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    print("Api Call Working $lat --- $lng");
+  });
+}
